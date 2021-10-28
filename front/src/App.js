48ef4d3204b9c8d5cb92535877c927b2ba4d6863
multicampus <plain3785@gmail.com>
2021-10-28 15:57:25 +0900

    feat: Add skeleton code (#S05P31F003-58)

---
+++ front/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom"; // 히스토리 모드 제거
+
+function App() {
+  return (
+    <div className="App">
+      <Router></Router>
+    </div>
+  );
+}
+
+export default App;
