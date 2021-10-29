cf4d7633df39c3abcfe63c2ec45446f493d31d59
multicampus <plain3785@gmail.com>
2021-10-29 15:50:32 +0900

    feat: Add sidebar component(#S05P31F003-64)

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom"; // 히스토리 모드 제거
 import Header from "./components/common/Header";
+import Sidebar from "./components/common/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Router></Router>
+      <Router>
+
+        <Sidebar/>
+      </Router>
     </div>
   );
 }
---
+++ front/src/components/common/Sidebar/styles.jsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+import { fonts, colors } from "../../../styles";
+
+export const Side = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 200px;
+  height: 100vh;
+  background-color: ${colors.white};
+`;
+
+export const Menu = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 40px 0 50px;
+  font-size: ${fonts.sm};
+  .username {
+    color: ${colors.black};
+    font-weight: 700;
+  }
+  .department {
+    color: ${colors.gray};
+  }
+`;
+export const ItemContainer = styled.div`
+  li {
+    position: relative;
+  }
+  li::before {
+    content: "";
+    position: absolute;
+    top: 1.7rem;
+    width: 0;
+    height: 2px;
+    background: ${colors.navy};
+  }
+
+  &:hover li::before {
+    width: 100%;
+    transition: width 0.3s cubic-bezier(0.22, 0.61, 0.36, 1);
+  }
+`;
+
+export const MenuItem = styled.li`
+  margin-bottom: 40px;
+  font-size: ${fonts.md};
+  :hover {
+    color: ${colors.black};
+  }
+`;
