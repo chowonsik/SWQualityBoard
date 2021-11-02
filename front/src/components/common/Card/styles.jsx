219f339af8d2ac01c02c674c7d2ebc2c36aadd83
kimmeh <mhko411@smail.kongju.ac.kr>
2021-11-02 13:44:17 +0900

    fix: remove margin in card component

--- front/src/components/common/Card/styles.jsx
+++ front/src/components/common/Card/styles.jsx
@@ -6,7 +6,6 @@ export const Card = styled.div`
   background-color: ${colors.white};
   padding: 0.5rem;
   border-radius: 12px;
-  margin: 3rem;
   filter: drop-shadow(
     0 0 0.2rem ${(props) => (props.isLight ? colors.orange : colors.gray)}
   );
