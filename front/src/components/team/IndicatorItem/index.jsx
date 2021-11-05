a9e23392f408bcb42cc64f7300ac986f2ef24886
multicampus <plain3785@gmail.com>
2021-11-05 16:15:34 +0900

    refactor: reflect on codereview about IndicatorItem component

--- front/src/components/team/IndicatorItem/index.jsx
+++ front/src/components/team/IndicatorItem/index.jsx
@@ -8,21 +8,24 @@ function IndicatorItem({
   standard = [],
   valueColor = "",
 }) {
-  let emojiCase;
-  const valueNum = parseInt(value.replace(/[^0-9]/g, ""));
-  if (valueNum >= standard[0]) {
-    emojiCase = <EmojiSmile className="emoji green" />;
-  } else if (valueNum >= standard[1]) {
-    emojiCase = <EmojiNeutral className="emoji yellow" />;
-  } else {
-    emojiCase = <EmojiFrown className="emoji red" />;
+  function getEmojiCase() {
+    let emojiCase;
+    const valueNum = parseInt(value.replace(/[^0-9]/g, ""));
+    if (valueNum >= standard[0]) {
+      emojiCase = <EmojiSmile className="emoji green" />;
+    } else if (valueNum >= standard[1]) {
+      emojiCase = <EmojiNeutral className="emoji yellow" />;
+    } else {
+      emojiCase = <EmojiFrown className="emoji red" />;
+    }
+    return emojiCase;
   }
 
   return (
     <Wrapper>
       <dt className="indicator-name">{name}</dt>
       <dd className={"indicator-value " + (valueColor && valueColor)}>
-        {emoji && emojiCase}
+        {emoji && getEmojiCase()}
         <span>{value}</span>
       </dd>
     </Wrapper>
