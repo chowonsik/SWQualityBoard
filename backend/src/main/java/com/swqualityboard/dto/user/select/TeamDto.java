dd4383927e592ca8748610f3cf4b2b1b0e49e158
wscho94 <wscho94@gmail.com>
2021-11-09 23:05:32 +0900

    feat: Add select user api (#S05P31F003-91)

---
+++ backend/src/main/java/com/swqualityboard/dto/user/select/TeamDto.java
@@ -0,0 +1,13 @@
+package com.swqualityboard.dto.user.select;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TeamDto {
+    private String id;
+    private String name;
+}
