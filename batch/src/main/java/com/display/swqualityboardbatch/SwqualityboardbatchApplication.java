8258d06e018cef12d9d20dcdfb3994f61d78e26f
wscho94 <wscho94@gmail.com>
2021-11-16 16:34:18 +0900

    fix: spring batch job and data (#S05P31F003-112)

--- batch/src/main/java/com/display/swqualityboardbatch/SwqualityboardbatchApplication.java
+++ batch/src/main/java/com/display/swqualityboardbatch/SwqualityboardbatchApplication.java
@@ -3,6 +3,7 @@ package com.display.swqualityboardbatch;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableBatchProcessing
@@ -11,7 +12,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class SwqualityboardbatchApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SwqualityboardbatchApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(SwqualityboardbatchApplication.class, args);
+        SpringApplication.exit(ctx);
     }
 
 }
