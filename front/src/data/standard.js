3131941d7eba695b5a02f48f64fc13437c75ab95
dwbyun16 <dwbyun16@gmail.com>
2021-11-18 15:57:48 +0900

    fix: Fix system standard (#S05P31F003-123)

--- front/src/data/standard.js
+++ front/src/data/standard.js
@@ -1,15 +1,15 @@
 export const standard = {
   system: {
-    구조품질지수: { complexity: 3, overlapping: 3, scale: 3 },
+    구조품질지수: { complexity: -1, overlapping: -1, scale: -1 },
     중대결함수: {
       critical: 1,
       high: 3,
       medium: 5,
       low: 7,
     },
-    기능적합성: [90, 80],
-    테스트커버리지: [90, 80],
-    시스템신뢰도: [90, 80],
+    기능적합성: [80, 60],
+    테스트커버리지: [80, 60],
+    시스템신뢰도: [600, 300],
   },
   team: {
     코드리뷰율: [90, 80],
